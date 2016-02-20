ccca35aacc949a19439e3cc4a43e9e70fc936a74
John Russell <j362r647@ku.edu>
2016-02-19 20:47:32 -0600

    enable happy dog

--- PassVal.js
+++ PassVal.js
@@ -7,12 +7,13 @@ var validate=function(/*does anything go here?*/ )
 	var firstPass=document.getElementById("passOne");
 	var secondPass=document.getElementById("passTwo");
 	
-	
-	
+	var happyDog=document.getElementById("soHappy");
+	happyDog.style.visibility="hidden";
 	if(firstPass.value==secondPass.value && secondPass.value.length>=8 && firstPass.value.length>=8)
 	{
 		alert("You did it! Hooray! Have a happy retriever.");
 		
+		happyDog.style.visibility="visible";
 	}
 	else if (firstPass.value==secondPass.value &&(firstPass.value.length<8||secondPass.value.length<8))
 	{
