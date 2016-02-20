4894df3242f32aed6a28882cea5bf2dcf7f043e4
John Russell <j362r647@ku.edu>
2016-02-20 13:58:31 -0600

    Make slideshow function, realize that there should only be one image tag. Total rework incoming.

--- SlideShow/SliSho.js
+++ SlideShow/SliSho.js
@@ -1,58 +1,87 @@
 
-
+var pic0;
+var pic1;
+var pic2;
+var pic3;
+var pic4;
 var loc;
+var picArray;
+
+var initialize=function()
+{
+	pic0=document.getElementById("image0");
+	pic1=document.getElementById("image1");
+	pic2=document.getElementById("image2");
+	pic3=document.getElementById("image3");
+	pic4=document.getElementById("image4");
+	picArray=[pic0,pic1,pic2,pic3,pic4];
+}
+
 var increment=function()
 {
-	var pic0=document.getElementById("image0");
-	var pic1=document.getElementById("image1");
-	var pic2=document.getElementById("image2");
-	var pic3=document.getElementById("image3");
-	var pic4=document.getElementById("image4");
-	
-	
-	//starts from zero, as usual
-	var picArray=[pic0,pic1,pic2,pic3,pic4];
-	//console.log(picArray);
-	loc=findLoc(picArray);
+	findLoc();
 	console.log(loc);
+	if(loc<4)
+	{
+		picArray[loc].height="0";
+		picArray[loc].width="0";
+		picArray[loc+1].height="500";
+		picArray[loc+1].width="500";
+	}
+	else
+	{
+		picArray[loc].height="0";
+		picArray[loc].width="0";
+		picArray[0].height="500";
+		picArray[0].width="500";
+	}
 	
-	//alert("making sure this works");
 }
 
 var decrement=function()
 {
 	findLoc();
-	//alert("DOes this work too?");
-}
-
-var findLoc=function(picArray)
-{
-	
-	
-	if(picArray[0].height=="500")
+	console.log(loc);
+	if(loc>0)
 	{
-		return(0);
-	}
-	else if(picArray[1].height=="500")
-	{
-		return(1);
-	}
-	else if(picArray[2].height=="500")
-	{
-		return(2);
-	}
-	else if(picArray[3].height=="500")
-	{
-		return(3);
-	}
-	else if(picArray[4].height=="500")
-	{
-		return(4);
+		picArray[loc].height="0";
+		picArray[loc].width="0";
+		picArray[loc-1].height="500";
+		picArray[loc-1].width="500";
 	}
 	else
 	{
-		return("something went wrong");
+		picArray[loc].height="0";
+		picArray[loc].width="0";
+		picArray[4].height="500";
+		picArray[4].width="500";
 	}
-	//alert(loc);
-	
+}
+
+var findLoc=function()
+{
+	if(picArray[0].height=="500")
+	{
+		loc=0;
+	}
+	else if(picArray[1].height=="500")
+	{
+		loc=1;
+	}
+	else if(picArray[2].height=="500")
+	{
+		loc=2;
+	}
+	else if(picArray[3].height=="500")
+	{
+		loc=3;
+	}
+	else if(picArray[4].height=="500")
+	{
+		loc=4;
+	}
+	else
+	{
+		alert("something's wrong");
+	}
 }
