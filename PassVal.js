d7ee49e7ed585a647d58e4e0b005375533278a9e
John Russell <j362r647@ku.edu>
2016-02-19 20:13:12 -0600

    Figure out how to retrieve submission box data via .value function

--- PassVal.js
+++ PassVal.js
@@ -3,4 +3,24 @@ var validate=function(/*does anything go here?*/ )
 	//Compare the two entries
 	//if same alert.whatever "good"
 	//else "they're not the same, dummy"
+	
+	var firstPass=document.getElementById("passOne");
+	var secondPass=document.getElementById("passTwo");
+	
+	//alert(secondPass.length);
+	//console.log(firstPass.value);
+	if(secondPass.value.length < 8)
+	{
+		alert("Password must be at least 8 characters.");
+	}
+	
+	if(firstPass==secondPass)
+	{
+		alert("Congratulations");
+	}
+	else
+	{
+		alert("You're an idiot");
+	}
+	
 }
